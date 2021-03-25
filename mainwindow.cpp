79209ebed3f239178fd13141571df8f4ead7f366
Ry3der <74059611+Ry3der@users.noreply.github.com>
2021-03-25 13:37:19 +0000

    Add files via upload
    Very Basic GUI created, allows the user to choose between Starting the game and viewing a brief explanation of the games mechanics, should probably be cleaned up to look more appealing. UmiBackGround.jpg is a file I was considering using as the background for the Main Menu, will look into implementing.

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+    ui->GameWidget->setCurrentIndex(0);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_ExitButton_clicked()
+{
+    QCoreApplication::quit();
+}
+
+void MainWindow::on_HTPButton_clicked()
+{
+    ui->GameWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_StartButton_clicked()
+{
+    ui->GameWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_ReturnButton_clicked()
+{
+     ui->GameWidget->setCurrentIndex(0);
+}
