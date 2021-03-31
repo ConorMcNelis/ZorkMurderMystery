09b1f93ca859b65ea7a092a83ed11d2e43210cdd
ConorMcNelis <60985221+ConorMcNelis@users.noreply.github.com>
2021-03-31 16:49:59 +0100

    Update and rename Zork_first_version/ZorkUL.h to main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "Command.h"
+#include "Room.h"
+#include "item.h"
+#include "Character.h"
+//#include "MainCharacter.h"
+#include <iostream>
+#include <QString>
+using namespace std;
+
+class Main {
+
+public:
+    Main();
+};
+
+#endif // MAIN_H
