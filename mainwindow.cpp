0659612d377a0b6b47586e03ae27f9da7fc50c7d
Ry3der <74059611+Ry3der@users.noreply.github.com>
2021-04-06 13:29:28 +0100

    Add files via upload
    -Updates to the UI
    -Basic Inventory created

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,6 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-
     ui->setupUi(this);
     ui->GameWidget->setCurrentIndex(0);
 }
@@ -56,68 +55,40 @@ void MainWindow::on_GoSouthButton_clicked(){
 }
 
 void MainWindow::on_ExamineButton_clicked(){
-    Items = currentRoom->getItemList();
-    Characters = a->getCharacterList();
-    it = Characters.begin();
-    for(int i = 0; i < currentRoom->numberOfItems(); i++){
+    vector<Item> Items = currentRoom->getItemList();
+    for(int i = 0; i < currentRoom->numberOfItems(); i++)
         if (ui->ItemList->currentText() == Items[i].getShortDescription())
             ui->Descriptions->setText(Items[i].getExamination());
-        else if(i < Characters.size())
-            if (ui->ItemList->currentText() == Characters[i].shortDescription()){
-                it += i;
-                currentCharacter = &(*it);
-                MainLobbyMethod(currentCharacter);
-            }
-    }
-}
-
-void MainWindow::on_Option1_clicked(){
-    ui->DialogueBox->setText(currentCharacter->getDialogue(2));
-}
-
-void MainWindow::on_Option2_clicked(){
-    ui->DialogueBox->setText(currentCharacter->getDialogue(3));
-}
-
-void MainWindow::on_Option3_clicked(){
-    ui->DialogueBox->setText(currentCharacter->getDialogue(4));
-}
-
-void MainWindow::on_BackButton_clicked(){
-    ui->GameWidget->setCurrentIndex(2);
 }
 
 void MainWindow::createRooms()  {
     //MainCharacter *player;
-    Room *b, *c, *d, *e, *f, *g, *h, *i, *j;
+    Room *a, *b, *c, *d, *e, *f, *g, *h, *i, *j;
     Item *soup, *rustedSword, *rock, *stick;
-    Character *Gardener, *Butler;
-
+    Character *Gardener;
 
     //Create Items
-    soup = new Item("soup", 10, 2);
-        soup->setLongDescription("On the oaken table you see a bowl of soup, steaming and filling the room with a pungent odour.");
-        soup->setExamination(readFile(":/Examinations/Text/Examinations/SoupExamination.txt"));
-    rustedSword = new Item("sword", 10, 2);
+    soup = new Item("Soup", 10, 2);
+        soup->setLongDescription("A bowl of soup with a terrible odor.");
+        soup->setExamination(readFile(":/CharacterLongDescriptions/Text/SoupExamination.txt"));
+    rustedSword = new Item("Sword", 10, 2);
         rustedSword->setLongDescription(readFile(":/CharacterLongDescriptions/Text/Sword.txt"));
-    stick = new Item("stick", 1, 11);
-        stick->setLongDescription("A broken treebranch lays snapped in the grass, it could be used as a crude weapon. ");
-        ui->Inventory->addItem(stick->getShortDescription());
-    rock = new Item("rock", 2, 22);
+    stick = new Item("Stick", 1, 11);
+        stick->setLongDescription("A wooden stick with some heft to it ");
+    rock = new Item("Rock", 2, 22);
         rock->setLongDescription("A heavy rock is wedged in the dirt, it could fit in the palm of your hand. ");
+        Inventory[1] = soup;
+        Inventory[2] = rustedSword;
+        Inventory[3] = stick;
 
     //create Characters
     Gardener = new Character("The Gardener");
-        Gardener->setDialogueLocation(":/Examinations/Dialogue/Text/Examinations/Character Dialogue/Gardener Dialogue.txt");
-    Butler = new Character("The Butler");
-        Butler->setDialogueLocation(":/Examinations/Dialogue/Text/Examinations/Character Dialogue/Butler Dialogue.txt");
+        ui->Descriptions->setText(Gardener->longDescription());
 
-    a = new MainLobby("The Main Lobby:");
+    a = new Room("The Main Lobby:");
         a->addItem(stick);
         a->addItem(rock);
-        a->addCharacter(Gardener);
-        a->addCharacter(Butler);
-    b = new Room("b");
+    b = new Room("");
     c = new Room("c");
     d = new Room("d");
         d->addItem(new Item("xx", 3, 33));
@@ -145,9 +116,8 @@ void MainWindow::createRooms()  {
     i->setExits(NULL, d, NULL, NULL);
     j->setExits(NULL, NULL, f, NULL);
 
-       currentRoom = b;
-
-       go("west");
+       currentRoom = a;
+       printWelcome();
 }
 
 void MainWindow::printWelcome() {
@@ -163,33 +133,18 @@ void MainWindow::go(string direction) {
     //Move to the next room
     Room* nextRoom = currentRoom->nextRoom(direction);
     if (nextRoom == NULL)
-        ui->Descriptions->setText("There's nothing in that direction\n"+currentRoom->longDescription()+"\n");
+        int x = 1;
+        //return("direction null");
     else
     {
-        //ui->Descriptions->setText("Current Room: "+currentRoom->shortDescription()+"\nNext Room: "+nextRoom->shortDescription());
        currentRoom = nextRoom;
+       vector<Item> Items = currentRoom->getItemList();
        ui->Descriptions->setText(currentRoom->longDescription()+"\n");
-       if (currentRoom->shortDescription() != "" ){
-           //ui->Descriptions->setText("This isn't the main lobby");
-           vector<Item> Items = currentRoom->getItemList();
-           ui->ItemList->clear();
-           for(int i = 0; i < currentRoom->numberOfItems(); i++)
-               ui->ItemList->addItem(Items[i].getShortDescription());
-       }
-       else{
-           MainLobby *CurrentRoom = a;
-           vector<Character> Characters = CurrentRoom->getCharacterList();
-           ui->Descriptions->setText(CurrentRoom->longDescription()+"\n");
-           ui->ItemList->clear();
-           for(int i = 0; i < CurrentRoom->numberOfCharacters(); i++)
-               ui->ItemList->addItem(Characters[i].shortDescription());
-       }
+       ui->ItemList->clear();
+       for(int i = 0; i < currentRoom->numberOfItems(); i++)
+           ui->ItemList->addItem(Items[i].getShortDescription());
+      // return currentRoom->longDescription();
     }
-}
-
-void MainWindow::MainLobbyMethod(Character *suspect){
-    ui->GameWidget->setCurrentIndex(3);
-    ui->DialogueBox->setText(suspect->getDialogue(0));
 }
 
 QString MainWindow::readFile(QString fileLocation){
@@ -215,5 +170,22 @@ QString MainWindow::readFile(QString fileLocation){
 }
 
 
+void MainWindow::on_ItemList_currentIndexChanged(int x)
+{
 
+   if(x == 0)
+   {
+        ui->InventoryDisplay->setText("Item Descriptions");
+   }
+   else
+   {
+    for(int i = 1; i < 10; i++)
+    {
+         if(x == i)
+        {
+             ui->InventoryDisplay->setText(Inventory[i]->getLongDescription());
+         }
 
+   }
+   }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -9,9 +9,7 @@
 #include <QDebug>
 #include <QFile>
 #include <QTextStream>
-#include "Command.h"
 #include "Room.h"
-#include "MainLobby.h"
 #include "item.h"
 #include "Character.h"
 #include "main.h"
@@ -31,10 +29,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    MainLobby *a;
     Room *currentRoom;
     QFile *descriptionText;
-    Character *currentCharacter;
    // MainCharacter *player;
     void createRooms();
     void printWelcome();
@@ -44,8 +40,8 @@ public:
     void createCharacters();
     void displayCharacters();
     void go(string direction);
-    void MainLobbyMethod(Character *suspect);
     QString readFile(QString fileLocation);
+    Item *Inventory [10];
 
 private slots:
 
@@ -67,22 +63,11 @@ private slots:
 
     void on_ExamineButton_clicked();
 
-    void on_Option1_clicked();
 
-    void on_Option2_clicked();
-
-    void on_Option3_clicked();
-
-    void on_BackButton_clicked();
-
-    //void on_ItemList_clicked();
+    void on_ItemList_currentIndexChanged(int x);
 
 private:
     Ui::MainWindow *ui;
     //MainWindow();
-    void goRoom(Command command);
-    vector<Item> Items;
-    vector<Character> Characters;
-    vector<Character>::iterator it;
 };
 #endif // MAINWINDOW_H
